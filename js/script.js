de7ba543feb32611dfa70eaddff3d96ee72c9064
Skoropad <shady5324@gmail.com>
2020-10-08 21:44:59 +0300

    update slick

--- js/script.js
+++ js/script.js
@@ -14,6 +14,15 @@ $('.slick-slider').slick({
 	waitForAnimate: false,
 	responsive: [
 		{
+			breakpoint: 1352,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2,
+				infinite: true,
+				dots: true
+			}
+		},
+		{
 			breakpoint: 1165,
 			settings: {
 				slidesToShow: 2,
@@ -23,14 +32,14 @@ $('.slick-slider').slick({
 			}
 		},
 		{
-			breakpoint: 377,
+			breakpoint: 876,
 			settings: {
 				slidesToShow: 1,
 				slidesToScroll: 1,
 				infinite: true,
 				dots: true
 			}
-		}
+		},
 		// You can unslick at a given breakpoint now by adding:
 		// settings: "unslick"
 		// instead of a settings object
